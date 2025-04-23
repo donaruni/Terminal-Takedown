8f1bc1edde72a6187215dd7cae7637c4b52ce381
Noor <noorfswadi@gmail.com>
2025-04-24 11:54:03 +1200

    Implemented basic enemy AI

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    public float moveSpeed;
+
+    void Start(){
+        player = FindFirstObjectByType<PlayerMovement>().transform;
+    }
+    
+    void Update(){
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime); //Constantly move the enemy towards player
+    }
+
+}
