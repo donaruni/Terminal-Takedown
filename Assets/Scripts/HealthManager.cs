cc898036dd21041286a3f7e7467c90557068e335
autlenci <rsp4807@autuni.ac.nz>
2025-05-06 22:19:21 +1200

    Added Game Over Screen
    - Added a prototype game over screen

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,29 +7,31 @@ public class HealthManager : MonoBehaviour
 {
     public Image healthBar;
     public float healthAmount = 100f;
+    public GameObject playAgain;
+
+    private bool isDead = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if(playAgain != null)
+        {
+            playAgain.SetActive(false);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
-    { 
-        if (healthAmount <= 0) //CHANGE THIS PART TO WHAT YOU WANT TO HAPPEN IF THE PLAYER HEALTH DROPS TO 0 (PLAYER DIES!)
+    {
+        if (healthAmount <= 0 && !isDead) //CHANGE THIS PART TO WHAT YOU WANT TO HAPPEN IF THE PLAYER HEALTH DROPS TO 0 (PLAYER DIES!)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //for now it is set to reload the level from start after player dies.
-        }
-
-        if (Input.GetKeyDown(KeyCode.Return)) //TESTS FOR DAMAGE TAKING WITH (RETURN/ENTER KEY!) EDIT THIS IN FUTURE FOR HOW U WANT TO TAKE DAMAGE
-        {
-            TakeDamage(20); //edit this part for damage taken
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space)) //TESTS FOR HEALING WITH (SPACEBAR KEY!) EDIT THIS IN FUTURE FOR HOW U WANT TO HEAL DAMAGE TAKEN!
-        {
-            Heal(10); //edit this part for damage healed
+            isDead= true;
+            if (playAgain != null)
+            {
+                playAgain.SetActive(true);
+                Time.timeScale = 0f;
+            }
         }
     }
 
@@ -36,7 +39,7 @@ public class HealthManager : MonoBehaviour
     {
         healthAmount -= damage;
         healthBar.fillAmount = healthAmount / 100f;
-    }    
+    }
 
     public void Heal(float healingAmount)
     {
@@ -44,5 +47,11 @@ public class HealthManager : MonoBehaviour
         healthAmount = Mathf.Clamp(healthAmount, 0, 100);
 
         healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
